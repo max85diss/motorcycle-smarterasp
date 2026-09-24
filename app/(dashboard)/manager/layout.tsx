bfceb2c0b474a0c175bfe7f8d048345284114a07
Ananda <ana1985dissa@gmail.com>
2026-09-25 00:07:42 +0530

    disable layout navbar

--- app/(dashboard)/manager/layout.tsx
+++ app/(dashboard)/manager/layout.tsx
@@ -5,7 +5,7 @@ import Navbar from "@/components/manager/navbar";
     const user = await getCurrentUser();
     return ( 
      <div className="min-h-screen bg-gray-100">
-     <Navbar username={user?.username ?? ""} />
+     {/* <Navbar username={user?.username ?? ""} /> */}
       <div className="flex"> 
         {/* <Sidebar  /> */}
          <main className="flex-1 overflow-y-auto p-6">
